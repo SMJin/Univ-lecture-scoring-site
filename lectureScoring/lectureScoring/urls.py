80c3ae1a086b2c75a19464f4d9c1a202a2953b4a
SMJin <sstaw@naver.com>
2020-02-17 16:54:43 +0900

    create mainLecture app

--- lectureScoring/lectureScoring/urls.py
+++ lectureScoring/lectureScoring/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import mainLecture.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mainLecture.views.index, name='index'),
 ]
