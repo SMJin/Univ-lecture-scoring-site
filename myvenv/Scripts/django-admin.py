285d001919a43d8c4967e1943f5429bb07f3a068
SMJin <sstaw@naver.com>
2020-02-17 15:58:56 +0900

    first_environment

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\smj\desktop\univ-lecture-scoring-site\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
