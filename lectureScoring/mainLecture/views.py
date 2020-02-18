fc86760296cc65003f4b31d6584994d3e986485d
SMJin <sstaw@naver.com>
2020-02-18 16:10:01 +0900

    CRUD 중 READ 구현하기

--- lectureScoring/mainLecture/views.py
+++ lectureScoring/mainLecture/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Lectures_info
 
 def index(request):
@@ -6,7 +6,8 @@ def index(request):
     context = {'lecture': lecture_i}
     return render(request, 'index.html', context)
 
-def lecture_test(request):
-    lecture_i = Lectures_info.objects
-    context = {'lecture': lecture_i}
+def lecture_test(request, lecture_id):
+    lecture_more = get_object_or_404(Lectures_info, pk = lecture_id)
+    # lecture_i = Lectures_info.objects
+    context = {'lecture': lecture_more}
     return render(request, 'show_lecture_test.html', context)
