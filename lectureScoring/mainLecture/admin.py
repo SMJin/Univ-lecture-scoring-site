29cf062df2df01b62fa5b6c52eac3337c03890b1
SMJin <sstaw@naver.com>
2020-02-18 13:50:33 +0900

    admin 과 model 연결

--- lectureScoring/mainLecture/admin.py
+++ lectureScoring/mainLecture/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from mainLecture.models import Lectures_info
 from mainLecture.models import Professor_info
 from mainLecture.models import Show_lectures_in_comment
+
+admin.site.register(Lectures_info)
+admin.site.register(Professor_info)
+admin.site.register(Show_lectures_in_comment)
