bde2ab1a2ee77205f3dcb5bd30a4df8048483091
YoonSyuhyeok <cooltime15@likelion.org>
2020-02-18 17:37:47 +0900

    views.py professor 추가

--- lectureScoring/mainLecture/views.py
+++ lectureScoring/mainLecture/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Lectures_info
+from .models import Lectures_info, Professor_info
 
 def index(request):
     lecture_i = Lectures_info.objects
@@ -8,6 +8,7 @@ def index(request):
 
 def lecture_test(request, lecture_id):
     lecture_more = get_object_or_404(Lectures_info, pk = lecture_id)
+    professor_info = Professor_info.objects
     # lecture_i = Lectures_info.objects
-    context = {'lecture': lecture_more}
+    context = {'lecture': lecture_more, 'professor': professor_info}
     return render(request, 'show_lecture_test.html', context)
